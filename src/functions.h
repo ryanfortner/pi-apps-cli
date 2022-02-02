8c87d61f4adb81290c9d21a2a7bf2ff479afcba9
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-02-01 19:06:13 -0500

    v0.0.2 (testing package workflow)

--- src/functions.h
+++ src/functions.h
@@ -1,8 +1,8 @@
 #define APPNAME "pi-apps-cli"
-#define APPVER "0.0.1"
+#define APPVER "0.0.2"
 #define VER_MAJOR 0
 #define VER_MINOR 0
-#define VER_REVISION 1
+#define VER_REVISION 2
 
 void echo(char text[256]);
 int help(void);
