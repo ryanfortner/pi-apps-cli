5fdb9975f2a0dd0f483eaaf6177571c6ab658883
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-02-01 19:12:02 -0500

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -1,8 +1,8 @@
 #define APPNAME "pi-apps-cli"
-#define APPVER "0.0.2"
+#define APPVER "0.0.3"
 #define VER_MAJOR 0
 #define VER_MINOR 0
-#define VER_REVISION 2
+#define VER_REVISION 3
 
 void echo(char text[256]);
 int help(void);
