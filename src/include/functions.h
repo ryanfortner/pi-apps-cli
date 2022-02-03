b094b48357f6b327cda65fd64aa51d1ed824da48
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-02-03 13:30:14 -0500

    Many improvements to the use of CMake (#2)

--- src/include/functions.h
+++ src/include/functions.h
@@ -1,9 +1,3 @@
-#define APPNAME "pi-apps-cli"
-#define APPVER "0.0.3"
-#define VER_MAJOR 0
-#define VER_MINOR 0
-#define VER_REVISION 3
-
 void echo(char text[256]);
 int help(void);
 int about(void);
